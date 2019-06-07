8a77c70598605de0a3fe131cba653077d6187f54
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2019-06-06 19:24:44 -0700

    Setting up a scheduler fsm test.

---
+++ src/test/java/com/github/statemachine/SchedulerStateMachineTest.java
@@ -0,0 +1,10 @@
+package com.github.statemachine;
+
+/**
+ * Tests for an FSM for Jobs and Tasks in a Distributed Scheduler.
+ * 
+ * @author gaurav
+ */
+public final class SchedulerStateMachineTest {
+  // TODO
+}
